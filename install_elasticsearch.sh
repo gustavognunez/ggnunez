efa70555cc82d62df6f5124dd1f86f577751f57d
Gustavo Nunez <51544844+gustavognunez@users.noreply.github.com>
2020-11-19 23:30:56 -0300

    Update install_elasticsearch.sh

--- install_elasticsearch.sh
+++ install_elasticsearch.sh
@@ -9,23 +9,57 @@ clear
 
 echo "Running ..."
 rpm --import https://packages.elasticsearch.org/GPG-KEY-elasticsearch
-#content = "
-#[elasticsearch-1.4]
-#name=Elasticsearch repository for 6.x packages
-#baseurl=https://artifacts.elastic.co/packages/6.x/yum
-#gpgcheck=1
-#gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
-#enabled=1
-#autorefresh=1
-#type=rpm-md"
+content = "
+[elasticsearch]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=0
+autorefresh=1
+type=rpm-md"
 echo "[OK]"
 
-#echo $content >> /etc/yum.repos.d/elasticsearch.repo
+echo $content >> /etc/yum.repos.d/elasticsearch.repo
 
+    yum clean packages
+
+    yum clean all
+
+    yum clean all
+    
 echo "Installing elasticsearch ..."
+yum -y install --enablerepo=elasticsearch elasticsearch
 yum -y install elasticsearch
 
 echo "[OK]"
+
+yum install java-1.8.0-openjdk.x86_64
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.2-x86_64.rpm
+
+rpm -ivh elasticsearch-7.9.2-x86_64.rpm
+
+systemctl enable elasticsearch.service
+
+systemctl start elasticsearch.service
+
+vim /etc/elasticsearch/elasticsearch.yml
+
+Ajustar
+
+cluster.name: my-application
+network.host: localhost
+http.port: 9200
+
+vim /etc/elasticsearch/jvm.options
+
+Alterar
+
+-Djava.io.tmpdir=/var/log/elasticsearch
+
+systemctl start elasticsearch.service
+
 
 echo "Opening Ports On Firewall ..."
 
@@ -37,11 +71,8 @@ service iptables save
 
 echo "[OK]"
 
-chkconfig elasticsearch on
-
-# set min max memory variables
-export ES_MIN_MEM=5G
-export ES_MAX_MEM=5G
-
 # restart server
 service elasticsearch restart
+
+
+#https://github.com/elastic/elasticsearch/issues/57018
